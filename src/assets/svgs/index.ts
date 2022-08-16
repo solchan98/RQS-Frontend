5b822fda0195c5ff6f119768aed4341eb531a859
solchan98 <dev.sol0427@gmail.com>
2022-08-16 20:07:07 +0900

    feat: explore 페이지 spaceItem 뷰 작성 (디테일 리팩토링 필요)

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -2,3 +2,5 @@ export { ReactComponent as Cat } from './cat.svg';
 export { ReactComponent as Noti } from './notification.svg';
 export { ReactComponent as Search } from './search.svg';
 export { ReactComponent as Add } from './add-alt.svg';
+export { ReactComponent as Members } from './members.svg';
+export { ReactComponent as Question } from './question.svg';
--- src/pages/Explore/index.tsx
+++ src/pages/Explore/index.tsx
@@ -1,44 +1,32 @@
 import cs from './explore.module.scss';
+import { SpaceItem } from './SpaceItem';
 
 export const Explore = () => {
   return (
     <div className={cs.exploreContainer}>
       <div className={cs.exploreTop}>Explore</div>
       <ul className={cs.exploreCardList}>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li> <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li> <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
-        <li className={cs.exploreCard}>아이템</li>
+        <li className={cs.exploreCard}>
+          <SpaceItem />
+        </li>
+        <li className={cs.exploreCard}>
+          <SpaceItem />
+        </li>
+        <li className={cs.exploreCard}>
+          <SpaceItem />
+        </li>
+        <li className={cs.exploreCard}>
+          <SpaceItem />
+        </li>
+        <li className={cs.exploreCard}>
+          <SpaceItem />
+        </li>
+        <li className={cs.exploreCard}>
+          <SpaceItem />
+        </li>
+        <li className={cs.exploreCard}>
+          <SpaceItem />
+        </li>
       </ul>
     </div>
   );
