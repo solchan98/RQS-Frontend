94f6011d2b06506a474478b4b07cc3b9a4dbe9f0
solchan98 <dev.sol0427@gmail.com>
2022-08-19 02:18:34 +0900

    feat: 새로운 스페이스 생성 API 연결

--- src/components/CreateSpaceModal/index.tsx
+++ src/components/CreateSpaceModal/index.tsx
@@ -1,10 +1,14 @@
-import { ChangeEventHandler, useState } from 'react';
+import { ChangeEventHandler, FormEventHandler, useState } from 'react';
 
 import { Exit } from '../../assets/svgs';
 import { ModalTemplate } from '../ModalTemplate';
 
 import cx from 'classnames';
 import cs from './createSpaceModal.module.scss';
+import { createSpace } from '../../service/spaces';
+import { useSetRecoilState } from 'recoil';
+import { spaceListState } from '../../recoil/atoms/spaces';
+import { ISpace } from 'types/space';
 
 interface Props {
   useModal: { isOpen: boolean; openModal: () => void; closeModal: () => void };
@@ -12,9 +16,30 @@ interface Props {
 
 export const CreateSpaceModal = ({ useModal }: Props) => {
   const { isOpen, closeModal } = useModal;
-  const [visibility, setVisibility] = useState('public');
+  const setSpaceList = useSetRecoilState(spaceListState);
 
-  const onClickVisibility: ChangeEventHandler<HTMLInputElement> = (e) => setVisibility(e.currentTarget.value);
+  const [title, setTitle] = useState('');
+  const onChangeTitle: ChangeEventHandler<HTMLInputElement> = (e) => setTitle(e.currentTarget.value);
+
+  const [visibility, setVisibility] = useState(true);
+  const onClickVisibility: ChangeEventHandler<HTMLInputElement> = () => setVisibility((prev) => !prev);
+
+  const createSpaceSuccessHandler = (data: ISpace) => {
+    setSpaceList((prev) => ({
+      ...prev,
+      spaceList: [data, ...prev.spaceList],
+    }));
+  };
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    createSpace(title, visibility)
+      .then((data) => {
+        createSpaceSuccessHandler(data);
+        closeModal();
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <ModalTemplate isOpen={isOpen} closeModal={closeModal} portalClassName='createSpace'>
@@ -25,32 +50,25 @@ export const CreateSpaceModal = ({ useModal }: Props) => {
             <Exit />
           </button>
         </div>
-        <form id='createSpace' className={cs.main}>
+        <form id='createSpace' className={cs.main} onSubmit={onSubmit}>
           <span className={cs.subTitle}>Space Name</span>
-          <input className={cs.input} placeholder='스페이스의 제목을 입력하세요. :)' />
+          <input
+            className={cs.input}
+            value={title}
+            onChange={onChangeTitle}
+            placeholder='스페이스의 제목을 입력하세요. :)'
+          />
         </form>
         <div className={cs.bottom}>
           <div className={cs.visibility}>
-            <label className={cx(cs.label, visibility === 'public' && cs.selected)} htmlFor='public'>
+            <label className={cx(cs.label, visibility && cs.selected)} htmlFor='public'>
               공개
             </label>
-            <input
-              type='radio'
-              value='public'
-              id='public'
-              checked={visibility === 'public'}
-              onChange={onClickVisibility}
-            />
-            <label className={cx(cs.label, visibility === 'private' && cs.selected)} htmlFor='private'>
+            <input type='radio' id='public' checked={visibility} onChange={onClickVisibility} />
+            <label className={cx(cs.label, !visibility && cs.selected)} htmlFor='private'>
               비공개
             </label>
-            <input
-              type='radio'
-              value='private'
-              id='private'
-              checked={visibility === 'private'}
-              onChange={onClickVisibility}
-            />
+            <input type='radio' id='private' checked={!visibility} onChange={onClickVisibility} />
           </div>
           <button className={cs.createSpaceBtn} type='submit' form='createSpace'>
             생성하기
--- src/service/spaces.ts
+++ src/service/spaces.ts
@@ -3,6 +3,7 @@ import { baseApi } from './index';
 import { ISpace } from 'types/space';
 import { reissueAtk } from './member';
 
+const CREATE_NEW_SPACE = '/space';
 const GET_ASIDE_MY_SPACE_LIST = '/space/all';
 
 const getAsideMySpaceListApi = (email: string, lastSpace?: ISpace) => {
@@ -26,4 +27,22 @@ export const getAsideMySpaceList = (email: string, lastSpace: ISpace | undefined
   return getAsideMySpaceListApi(email, lastSpace && lastSpace)
     .then((data) => data)
     .catch(() => reissueAtk().then(() => getAsideMySpaceListApi(email, lastSpace)));
+};
+
+const createSpaceApi = (title: string, visibility: boolean) => {
+  const atk = store.get('atk');
+  const data = { title, visibility };
+  return baseApi
+    .post(CREATE_NEW_SPACE, data, {
+      headers: {
+        Authorization: `bearer ${atk}`,
+      },
+    })
+    .then((res) => res.data);
+};
+
+export const createSpace = (title: string, visibility: boolean) => {
+  return createSpaceApi(title, visibility)
+    .then((data) => data)
+    .catch(() => reissueAtk().then(() => createSpaceApi(title, visibility)));
 };
