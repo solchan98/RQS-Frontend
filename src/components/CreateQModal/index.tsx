361ee7556212f3400a69f396333c73c7ef84e42a
solchan98 <dev.sol0427@gmail.com>
2022-08-18 16:37:22 +0900

    feat: 키워드 추가 삭제 및 개수 제한에 따른 추가 제한 기능 구현

--- src/components/CreateQModal/index.tsx
+++ src/components/CreateQModal/index.tsx
@@ -4,13 +4,32 @@ import { Exit } from '../../assets/svgs';
 
 import cs from './createQModal.module.scss';
 import { ModalTemplate } from '../ModalTemplate';
+import { ChangeEventHandler, FormEventHandler, MouseEventHandler, useState } from 'react';
 
 interface Props {
   useModal: { isOpen: boolean; openModal: () => void; closeModal: () => void };
 }
 
+const HINT_LIST = ['보안', 'HTTP'];
+
 export const CreateQModal = ({ useModal }: Props) => {
   const { isOpen, closeModal } = useModal;
+
+  const [hint, setHint] = useState('');
+  const onChangeHint: ChangeEventHandler<HTMLInputElement> = (e) => setHint(e.currentTarget.value);
+
+  const [hintList, setHintList] = useState<string[]>(HINT_LIST);
+
+  const onSubmitAddHint: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    const exist = hintList.find((item) => item === hint);
+    if (!exist) setHintList((prev) => [...prev, hint]);
+    setHint('');
+  };
+  const onClickDeleteHint: MouseEventHandler<HTMLButtonElement> = (e) => {
+    const target = e.currentTarget.dataset.id;
+    setHintList((prev) => prev.filter((item) => item !== target));
+  };
 
   return (
     <ModalTemplate isOpen={isOpen} closeModal={closeModal} portalClassName='createQuestion'>
@@ -29,38 +48,23 @@ export const CreateQModal = ({ useModal }: Props) => {
         </form>
         <div className={cs.bottom}>
           <span className={cx(cs.subTitle, cs.bottomSubTitle)}>힌트로 사용할 키워드를 추가해보세요! (최대 5개 )</span>
-          <input className={cs.hintInput} placeholder='ex) 보안' />
+          <form onSubmit={onSubmitAddHint}>
+            <input
+              disabled={hintList.length >= 5}
+              className={cs.hintInput}
+              placeholder={hintList.length >= 5 ? '더이상 추가할 수 없습니다.' : 'Ex) 보안'}
+              value={hint}
+              onChange={onChangeHint}
+            />
+          </form>
           <ul className={cs.hintList}>
-            <li>
-              <button type='button' className={cs.hint}>
-                HTTP
-              </button>
-            </li>
-            <li>
-              <button type='button' className={cs.hint}>
-                보안
-              </button>
-            </li>
-            <li>
-              <button type='button' className={cs.hint}>
-                HTTP
-              </button>
-            </li>
-            <li>
-              <button type='button' className={cs.hint}>
-                보안
-              </button>
-            </li>
-            <li>
-              <button type='button' className={cs.hint}>
-                HTTP
-              </button>
-            </li>
-            <li>
-              <button type='button' className={cs.hint}>
-                보안
-              </button>
-            </li>
+            {hintList.map((h) => (
+              <li key={h}>
+                <button type='button' data-id={h} className={cs.hint} onClick={onClickDeleteHint}>
+                  {h}
+                </button>
+              </li>
+            ))}
           </ul>
           <div className={cs.createQBtnWrapper}>
             <button className={cs.createQBtn} type='submit' form='createQuestion'>
