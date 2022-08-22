2f9154ea2167faf72d941be292e029e35ae3501b
solchan98 <dev.sol0427@gmail.com>
2022-08-23 00:27:08 +0900

    feat: 스페이스 멤버 권한 변경 임시 뷰 작업 및 API 연결

--- src/pages/UpdateSpace/index.tsx
+++ src/pages/UpdateSpace/index.tsx
@@ -4,7 +4,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { ChangeEventHandler, FormEventHandler, MouseEventHandler, useState } from 'react';
 
 import { useLogout } from 'hooks/useLogout';
-import { updateSpaceTitle } from 'service/spaces';
+import { changeSpaceMemberRole, updateSpaceTitle } from 'service/spaces';
 import { spaceListState } from 'recoil/atoms/spaces';
 import { memberState } from 'recoil/atoms/member';
 import { ISpace } from 'types/space';
@@ -14,10 +14,12 @@ import cs from './updateSpace.module.scss';
 export const UpdateSpace = () => {
   const { spaceId } = useParams();
   const logout = useLogout();
+  const nav = useNavigate();
+  const memberValue = useRecoilValue(memberState);
 
   const [spaceListValue, setSpaceListValue] = useRecoilState(spaceListState);
-  const [space, setSpace] = useState<ISpace | undefined>(
-    spaceListValue.spaceList.find((s) => s.spaceId === Number(spaceId))
+  const [space, setSpace] = useState<ISpace>(
+    spaceListValue.spaceList.find((s) => s.spaceId === Number(spaceId)) ?? ({} as ISpace)
   );
 
   const [isTitleUpdate, setIsTitleUpdate] = useState(false);
@@ -42,9 +44,6 @@ export const UpdateSpace = () => {
       });
   };
 
-  const nav = useNavigate();
-  const memberValue = useRecoilValue(memberState);
-
   useMount(() => {
     const spaceMember = space?.spaceMemberList.find((s) => s.email === memberValue.email);
     if (!spaceMember || spaceMember.role !== 'ADMIN') {
@@ -53,26 +52,92 @@ export const UpdateSpace = () => {
     }
   });
 
+  const onSubmitChangeRole: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    const { id, role } = e.currentTarget.dataset;
+    if (!role) return;
+    changeSpaceMemberRole(Number(spaceId), Number(id), role)
+      .then((data) => {
+        const curSpace = spaceListValue.spaceList.find((s) => s.spaceId === Number(spaceId));
+        const changedMemberList =
+          curSpace?.spaceMemberList.map((sm) => (sm.spaceMemberId === Number(id) ? data : sm)) ?? [];
+        setSpaceListValue((prev) => ({
+          ...prev,
+          spaceList: prev.spaceList.map((s) =>
+            s.spaceId === Number(spaceId) ? { ...s, spaceMemberList: changedMemberList } : s
+          ),
+        }));
+        setSpace((prev) => ({
+          ...prev,
+          spaceMemberList: changedMemberList,
+        }));
+      })
+      .catch((err) => {
+        if (err.status === 401) logout();
+        alert(err.response?.data.message ?? 'SERVER ERROR');
+      });
+  };
+
   return (
     <div className={cs.container}>
       <div className={cs.top}>스페이스 관리</div>
       <div>
         {!isTitleUpdate && (
-          <>
+          <div>
             <div>{space?.title}</div>
             <button type='button' onClick={onChangeIsTitleUpdate}>
               변경
             </button>
-          </>
+          </div>
         )}
         {isTitleUpdate && (
           <form id='updateTitle' onSubmit={onSubmitTitleUpdate}>
             <input value={title} onChange={onChangeTitle} />
+            <button type='button' onClick={onChangeIsTitleUpdate}>
+              취소
+            </button>
             <button type='submit' form='updateTitle'>
               저장
             </button>
           </form>
         )}
+      </div>
+      <div>
+        <div>멤버 리스트</div>
+        <ul>
+          {space?.spaceMemberList.map((spaceMember) => (
+            <li key={spaceMember.spaceMemberId}>
+              <span>{spaceMember.email}</span>
+              <form
+                id={`changeRoleAdmin_${spaceMember.spaceMemberId}`}
+                data-role='ADMIN'
+                data-id={spaceMember.spaceMemberId}
+                onSubmit={onSubmitChangeRole}
+              >
+                <button type='submit' form={`changeRoleAdmin_${spaceMember.spaceMemberId}`}>
+                  ADMIN
+                  {spaceMember.role === 'ADMIN' && ' O'}
+                </button>
+              </form>
+              <form
+                id={`changeRoleMember_${spaceMember.spaceMemberId}`}
+                data-role='MEMBER'
+                data-id={spaceMember.spaceMemberId}
+                onSubmit={onSubmitChangeRole}
+              >
+                <button type='submit' data-id='MEMBER' form={`changeRoleMember_${spaceMember.spaceMemberId}`}>
+                  MEMBER
+                  {spaceMember.role === 'MEMBER' && ' O'}
+                </button>
+              </form>
+              <form id='deleteMember'>
+                <button type='submit' form='deleteMember'>
+                  제외
+                </button>
+              </form>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
--- src/service/spaces.ts
+++ src/service/spaces.ts
@@ -6,6 +6,7 @@ import { reissueAtk } from './member';
 const CREATE_NEW_SPACE = '/space';
 const GET_ASIDE_MY_SPACE_LIST = '/space/all';
 const UPDATE_SPACE_TITLE = '/space';
+const UPDATE_SPACE_MEMBER_ROLE = '/space/spaceMember/role';
 
 const getAsideMySpaceListApi = (email: string, lastSpace?: ISpace) => {
   const atk = store.get('atk');
@@ -64,4 +65,23 @@ export const updateSpaceTitle = (spaceId: number, title: string) => {
   return updateSpaceTitleApi(spaceId, title)
     .then((data) => data)
     .catch(() => reissueAtk().then(() => updateSpaceTitleApi(spaceId, title)));
+};
+
+const changeSpaceMemberRoleApi = (spaceId: number, spaceMemberId: number, role: string) => {
+  const atk = store.get('atk');
+  const params = { spaceId, spaceMemberId, role };
+  return baseApi
+    .patch(UPDATE_SPACE_MEMBER_ROLE, null, {
+      params,
+      headers: {
+        Authorization: `bearer ${atk}`,
+      },
+    })
+    .then((res) => res.data);
+};
+
+export const changeSpaceMemberRole = (spaceId: number, spaceMemberId: number, role: string) => {
+  return changeSpaceMemberRoleApi(spaceId, spaceMemberId, role)
+    .then((data) => data)
+    .catch(() => reissueAtk().then(() => changeSpaceMemberRoleApi(spaceId, spaceMemberId, role)));
 };
