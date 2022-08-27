8c7e4726e02e0185abaa9c3fab6744fb10e49484
solchan98 <dev.sol0427@gmail.com>
2022-08-28 00:56:53 +0900

    refactor: React-Query Retry 속성 해제

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient({
   defaultOptions: {
-    queries: { refetchOnWindowFocus: false },
+    queries: { refetchOnWindowFocus: false, retry: false },
   },
 });
 
