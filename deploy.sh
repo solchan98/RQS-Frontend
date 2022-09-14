4ea4a1a5173221a2a5c939817bf2f6457b701e68
solchan98 <dev.sol0427@gmail.com>
2022-09-14 17:45:36 +0900

    refactor: 빌드 파일 버킷 업로드 관련 수정 반영

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 sudo rm -rf build
-sudo mkdir build
-sudo gcloud alpha storage cp gs://front-build/build/* ./build
+sudo gcloud alpha storage cp gs://front-build/build.tar
+sudo tar -xvf build.tar
 sudo systemctl reload nginx
