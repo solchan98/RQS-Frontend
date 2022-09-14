a3590dcbd3f033d0ee5951b1114610701760efe9
solchan98 <dev.sol0427@gmail.com>
2022-09-14 17:51:27 +0900

    refactor: 배포 쉘 잘못된 부분 수정 반영

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 sudo rm -rf build
-sudo gcloud alpha storage cp gs://front-build/build.tar
+sudo gcloud alpha storage cp gs://front-build/build.tar ./
 sudo tar -xvf build.tar
 sudo systemctl reload nginx
