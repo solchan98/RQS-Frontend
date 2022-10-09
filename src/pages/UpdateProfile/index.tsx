4ae2678996c5de568d8043aa3f864a955360b6c6
solchan98 <dev.sol0427@gmail.com>
2022-10-09 20:50:51 +0900

    feat: 프로필(닉네임) 변경 API 연결

--- src/pages/UpdateProfile/index.tsx
+++ src/pages/UpdateProfile/index.tsx
@@ -1,10 +1,14 @@
-import { ChangeEventHandler, useState } from 'react';
-import { AxiosError } from 'axios';
-import { useQuery } from '@tanstack/react-query';
+import { ChangeEventHandler, FormEventHandler, useState } from 'react';
+import store from 'store';
+import jwtDecode from 'jwt-decode';
+import { useMount } from 'react-use';
+import { useSetRecoilState } from 'recoil';
 import { useNavigate, useParams } from 'react-router-dom';
 
-import { useRecoilState } from 'recoil';
+import { useLogout } from 'hooks/useLogout';
+import { getMemberInfo, updateMemberInfo } from 'service/member';
 import { memberState } from 'recoil/atoms/member';
+import { IMember } from 'types/member';
 
 import cs from './updateProfile.module.scss';
 
@@ -12,35 +16,51 @@ export const UpdateProfile = () => {
   const { memberId } = useParams();
   const [isAccessible, setIsAccessible] = useState(true);
 
-  const [member, setMember] = useRecoilState(memberState);
-  const [nickname, setNickname] = useState(member.nickname); // TODO: api call the GET_MEMBER_INFO
+  const setMemberState = useSetRecoilState(memberState);
+  const [avatar, setAvatar] = useState('');
+  const [nickname, setNickname] = useState(''); // TODO: api call the GET_MEMBER_INFO
 
   const onChangeNickname: ChangeEventHandler<HTMLInputElement> = (e) => setNickname(e.currentTarget.value);
+  const nav = useNavigate();
+  const getSubjectInAtk = () => {
+    const atk = store.get('atk');
+    const decoded: { exp: number; iat: number; sub: string } = jwtDecode(atk);
+    return JSON.parse(decoded.sub);
+  };
+  const validate = (subject: { memberId: number }) => {
+    if (subject.memberId !== Number(memberId)) {
+      alert('접근할 수 없습니다.');
+      nav(-1);
+    }
+  };
 
-  // const onSuccessCheckRequest = (accessible: boolean) => {
-  //   setIsAccessible(accessible);
-  //   if (!accessible) {
-  //     alert('접근할 수 없습니다.');
-  //     nav(-1);
-  //   }
-  // };
-  //
-  // const nav = useNavigate();
-  // const logout = useLogout();
-  // const onError = (err: AxiosError<{ message: string }>) => {
-  //   if (err.response?.status === 401) {
-  //     logout();
-  //   } else {
-  //     nav(-1);
-  //     alert(err.response?.data.message);
-  //   }
-  // };
-  //
-  // useQuery([`$member_verify_${memberId}`], () => checkIsUpdatable(), {
-  //   select: (accessible): boolean => accessible,
-  //   onSuccess: (accessible) => onSuccessCheckRequest(accessible),
-  //   onError: (err: AxiosError<{ message: string }>) => onError(err),
-  // });
+  const logout = useLogout();
+  const setupMember = () => {
+    getMemberInfo()
+      .then((data) => setNickname(data.nickname))
+      .catch((err) => (err.response?.status === 401 ? logout() : alert(err.response?.data.message)));
+  };
+
+  useMount(() => {
+    const subject = getSubjectInAtk();
+    validate(subject);
+    setIsAccessible(true);
+    setupMember();
+  });
+
+  const onSuccessUpdateProfile = (data: IMember) => {
+    setNickname(data.nickname);
+    setMemberState(data);
+    alert('프로필이 변경되었습니다.');
+    nav(-1);
+  };
+
+  const onSubmitMemberUpdate: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    updateMemberInfo(nickname)
+      .then((data) => onSuccessUpdateProfile(data))
+      .catch((err) => (err.response?.status === 401 ? logout() : alert(err.response?.data.message)));
+  };
 
   if (!isAccessible) return <div>권한 확인중...</div>;
 
@@ -48,7 +68,7 @@ export const UpdateProfile = () => {
     <div className={cs.container}>
       <div className={cs.title}>프로필 수정</div>
       <div className={cs.top}>
-        <img src={member.avatar} alt='avatar' />
+        <img src={avatar} alt='avatar' />
         <div className={cs.updateAvatar}>
           <button type='button'>이미지 바꾸기</button>
           <form id='updateAvatar'>
@@ -59,7 +79,7 @@ export const UpdateProfile = () => {
         </div>
       </div>
       <div className={cs.middle}>
-        <form id='updateProfile'>
+        <form id='updateProfile' onSubmit={onSubmitMemberUpdate}>
           <div className={cs.updateInfoWrapper}>
             <span className={cs.label}>Nickname</span>
             <input type='text' value={nickname} onChange={onChangeNickname} />
