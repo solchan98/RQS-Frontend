cd587947aa1d9f9e25c764d3f61a017381a69fe4
solchan98 <dev.sol0427@gmail.com>
2022-11-16 15:08:15 +0900

    refactor: 아이템 제목 생성 및 수성 textarea 스타일 및 ToastEditor 스타일 수정

--- src/components/ToastUI/Editor/index.tsx
+++ src/components/ToastUI/Editor/index.tsx
@@ -1,6 +1,7 @@
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { forwardRef } from 'react';
+import cs from './editor.module.scss';
 
 interface Props {
   placeHolder: string;
@@ -9,21 +10,23 @@ interface Props {
 
 const ToastEditor = forwardRef<Editor, Props>(({ placeHolder, initialContent }, ref) => {
   return (
-    <Editor
-      ref={ref} // DOM 선택용 useRef
-      placeholder={placeHolder}
-      previewStyle='tab' // 미리보기 스타일 지정
-      initialEditType='markdown' //
-      hideModeSwitch
-      toolbarItems={[
-        // 툴바 옵션 설정
-        ['heading', 'bold'],
-        ['ul', 'link'],
-        ['code'],
-      ]}
-      useCommandShortcut={false} // 키보드 입력 컨트롤 방지 v
-      initialValue={initialContent}
-    />
+    <div className={cs.container}>
+      <Editor
+        ref={ref} // DOM 선택용 useRef
+        placeholder={placeHolder}
+        previewStyle='tab' // 미리보기 스타일 지정
+        initialEditType='markdown' //
+        hideModeSwitch
+        toolbarItems={[
+          // 툴바 옵션 설정
+          ['heading', 'bold'],
+          ['ul', 'link'],
+          ['code'],
+        ]}
+        useCommandShortcut={false} // 키보드 입력 컨트롤 방지 v
+        initialValue={initialContent}
+      />
+    </div>
   );
 });
 
