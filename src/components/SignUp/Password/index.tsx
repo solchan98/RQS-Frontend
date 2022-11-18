f9fd56b4847b216360a0c3aa952b39a55fdb88e4
solchan98 <dev.sol0427@gmail.com>
2022-11-18 17:33:10 +0900

    feat: 회원가입 프로세스 비밀번호 단계 컴포넌트 구현 및 전체 프로세스 연결 완료

--- src/components/SignUp/Password/index.tsx
+++ src/components/SignUp/Password/index.tsx
@@ -1,23 +1,47 @@
 import { Zoom } from '@mui/material';
+import { ChangeEventHandler, Dispatch, SetStateAction, useState } from 'react';
 
+import cx from 'classnames';
 import cms from '../common.module.scss';
 
 interface Props {
+  password: string;
+  setPassword: Dispatch<SetStateAction<string>>;
   checked: boolean;
-  nextStep: () => void;
 }
 
-const Password = ({ checked, nextStep }: Props) => {
-  const onSubmitHandler = () => {
-    // TODO: validation
-    nextStep();
+const PASSWORD_REG_EXP = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@!%*#?&])[A-Za-z\d$@!%*#?&]{8,}$/;
+
+const Password = ({ password, setPassword, checked }: Props) => {
+  const [invalidPassword, setInvalidPassword] = useState(true);
+  const onChangePassword: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setInvalidPassword(!PASSWORD_REG_EXP.test(e.currentTarget.value));
+    setPassword(e.currentTarget.value);
   };
+
   return (
     <Zoom style={{ display: checked ? 'block' : 'none' }} in={checked}>
       <div className={cms.container}>
-        <p className={cms.label}>Password</p>
         <div className={cms.inputContainer}>
-          <input className={cms.input} placeholder='비밀번호를 입력해주세요.' onKeyDown={onSubmitHandler} />
+          <p className={cms.label}>Password</p>
+          <input
+            type='password'
+            className={cms.input}
+            value={password}
+            placeholder='비밀번호를 입력해주세요.'
+            onChange={onChangePassword}
+          />
+          {invalidPassword && <p className={cms.alert}>영문과 특수문자 숫자를 포함하며 8자 이상이어야 합니다.</p>}
+          <div className={cms.btnWrapper}>
+            <button
+              type='submit'
+              className={cx(cms.signUpBtn, invalidPassword && cms.invalid)}
+              disabled={invalidPassword}
+              form='signUp'
+            >
+              회원 가입
+            </button>
+          </div>
         </div>
       </div>
     </Zoom>
--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -1,110 +1,50 @@
-import { ChangeEventHandler, FormEventHandler, useState } from 'react';
-
-import { checkEmail, signUp } from 'service/member';
-
-import cx from 'classnames';
-import cs from './signUp.module.scss';
-import { useNavigate } from 'react-router-dom';
 import { AxiosError } from 'axios';
 import { Box } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { FormEventHandler, useState } from 'react';
+
+import { signUp } from 'service/member';
 import Email from './Email';
 import Nickname from './Nickname';
 import Password from './Password';
 
-const EMAIL_REG_EXP = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-const PASSWORD_REG_EXP = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@!%*#?&])[A-Za-z\d$@!%*#?&]{8,}$/;
+import cs from './signUp.module.scss';
+
+const PROCESS_CNT = 3;
 
 export const SignUp = () => {
-  // const [email, setEmail] = useState('');
-  // const [invalidEmail, setInvalidEmail] = useState(false);
-  // const onChangeEmail: ChangeEventHandler<HTMLInputElement> = (e) => {
-  //   if (checkDuplicateEmail) setCheckDuplicateEmail((prev) => !prev);
-  //   setInvalidEmail(!EMAIL_REG_EXP.test(e.currentTarget.value));
-  //   setEmail(e.currentTarget.value);
-  // };
-  // const [checkDuplicateEmail, setCheckDuplicateEmail] = useState(false);
-  // const onCheckDuplicateEmail = () => {
-  //   if (email !== '' && !invalidEmail) {
-  //     checkEmail(email).then((data) => {
-  //       setCheckDuplicateEmail(!data.exist);
-  //       if (data.exist) alert('이미 존재하는 이메일입니다.');
-  //     });
-  //   }
-  // };
-  //
-  // const [nickname, setNickname] = useState('');
-  // const [invalidNickname, setInvalidNickname] = useState(false);
-  // const onChangeNickname: ChangeEventHandler<HTMLInputElement> = (e) => {
-  //   setInvalidNickname(e.currentTarget.value === '');
-  //   setNickname(e.currentTarget.value);
-  // };
-  //
-  // const [password, setPassword] = useState('');
-  // const onChangePassword: ChangeEventHandler<HTMLInputElement> = (e) => {
-  //   setInvalidPassword(!PASSWORD_REG_EXP.test(e.currentTarget.value));
-  //   setPassword(e.currentTarget.value);
-  // };
-  // const [invalidPassword, setInvalidPassword] = useState(false);
-  //
-  // const nav = useNavigate();
-  // const onSubmitSuccessHandler = () => {
-  //   alert('회원가입에 성공하였습니다.');
-  //   nav('/auth/login');
-  // };
-  //
-  // const onSubmitFailHandler = (err: AxiosError<{ message: string }>) => {
-  //   alert(err.response?.data.message ?? 'SERVER ERROR');
-  // };
-  //
-  // const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
-  //   e.preventDefault();
-  //   signUp(email, nickname, password)
-  //     .then(() => onSubmitSuccessHandler())
-  //     .catch((err) => onSubmitFailHandler(err));
-  // };
-
-  /// /////
-
   const [email, setEmail] = useState('');
   const [nickname, setNickname] = useState('');
+  const [password, setPassword] = useState('');
 
   const [curIdx, setCurIdx] = useState(0);
-
   const nextStep = () => {
-    if (curIdx + 1 === 3) setCurIdx(0);
+    if (curIdx + 1 === PROCESS_CNT) setCurIdx(0);
     else setCurIdx((prev) => prev + 1);
   };
 
+  const nav = useNavigate();
+  const onSubmitSuccessHandler = () => {
+    alert('회원가입에 성공하였습니다.');
+    nav('/auth/login');
+  };
+
+  const onSubmitFailHandler = (err: AxiosError<{ message: string }>) => {
+    alert(err.response?.data.message ?? 'SERVER ERROR');
+  };
+
+  const onSubmitSignUp: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    signUp(email, nickname, password).then(onSubmitSuccessHandler).catch(onSubmitFailHandler);
+  };
+
   return (
-    <form className={cs.container}>
-      <Box style={{ position: 'relative' }}>
+    <form id='signUp' className={cs.container} onSubmit={onSubmitSignUp}>
+      <Box>
         <Email email={email} setEmail={setEmail} checked={curIdx === 0} nextStep={nextStep} />
         <Nickname nickname={nickname} setNickname={setNickname} checked={curIdx === 1} nextStep={nextStep} />
-        <Password checked={curIdx === 2} nextStep={nextStep} />
+        <Password password={password} setPassword={setPassword} checked={curIdx === 2} />
       </Box>
-      {/* <input className={cs.input} type='email' value={email} placeholder='아이디' onChange={onChangeEmail} /> */}
-      {/* <button */}
-      {/*  className={cs.checkEmailBtn} */}
-      {/*  disabled={email === '' || invalidEmail || checkDuplicateEmail} */}
-      {/*  type='button' */}
-      {/*  onClick={onCheckDuplicateEmail} */}
-      {/* > */}
-      {/*  {checkDuplicateEmail ? '확인 완료 ✅' : '중복체크 ☑️'} */}
-      {/* </button> */}
-      {/* <span className={cx(cs.inputInfo, invalidEmail && cs.alert)}>이메일 형식으로 작성해주세요</span> */}
-      {/* <input className={cs.input} type='text' value={nickname} placeholder='닉네임' onChange={onChangeNickname} /> */}
-      {/* <span className={cx(cs.inputInfo, invalidNickname && cs.alert)}>닉네임은 비어있으면 안됩니다.</span> */}
-      {/* <input className={cs.input} type='password' value={password} placeholder='비밀번호' onChange={onChangePassword} /> */}
-      {/* <span className={cx(cs.inputInfo, invalidPassword && cs.alert)}> */}
-      {/*  영문과 특수문자 숫자를 포함하며 8자 이상이어야 합니다. */}
-      {/* </span> */}
-      {/* <button */}
-      {/*  className={cs.signUpBtn} */}
-      {/*  disabled={invalidEmail || invalidPassword || !checkDuplicateEmail || nickname === ''} */}
-      {/*  type='submit' */}
-      {/* > */}
-      {/*  회원가입 */}
-      {/* </button> */}
     </form>
   );
 };
