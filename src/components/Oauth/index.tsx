229fd935ac8be84cf5991f16bcaca46a3ecb9efc
solchan98 <dev.sol0427@gmail.com>
2023-03-11 21:46:38 +0900

    feat: oauth url 수정

--- src/components/Oauth/index.tsx
+++ src/components/Oauth/index.tsx
@@ -3,10 +3,10 @@ import cx from 'classnames';
 import { GoogleLogin, KakaoLogin } from 'assets/svgs';
 
 const google =
-  'https://accounts.google.com/o/oauth2/v2/auth?client_id=751204375055-hp0dl77isgfllta0c2ps76jlqg2ccsao.apps.googleusercontent.com&redirect_uri=http://localhost:3000/oauth/google&response_type=code&scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email';
+  'https://accounts.google.com/o/oauth2/v2/auth?client_id=528149101516-tktuilpiq62vkflrqvs83g118s7n1pm0.apps.googleusercontent.com&redirect_uri=https://quiz-box.shop/oauth/google&response_type=code&scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email';
 
 const kakao =
-  'https://kauth.kakao.com/oauth/authorize?client_id=4a07e318a99998cbe9c9c818bc51cfdf&redirect_uri=http://localhost:3000/oauth/kakao&response_type=code';
+  'https://kauth.kakao.com/oauth/authorize?client_id=cc1dfb0ff3ba6fc45a04a2771433fd75&redirect_uri=https://quiz-box.shop/oauth/kakao&response_type=code';
 
 export const Oauth = () => {
   return (
