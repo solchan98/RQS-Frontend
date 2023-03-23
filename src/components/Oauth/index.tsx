c70a233198e9aa933521d3afcaacdca51e9d2f85
solchan98 <dev.sol0427@gmail.com>
2023-03-23 16:45:38 +0900

    feat: oauth 로그인 url .env 환경변수로 수정

--- src/components/Oauth/index.tsx
+++ src/components/Oauth/index.tsx
@@ -2,20 +2,14 @@ import cs from './oauth.module.scss';
 import cx from 'classnames';
 import { GoogleLogin, KakaoLogin } from 'assets/svgs';
 
-const google =
-  'https://accounts.google.com/o/oauth2/v2/auth?client_id=528149101516-tktuilpiq62vkflrqvs83g118s7n1pm0.apps.googleusercontent.com&redirect_uri=https://quiz-box.shop/oauth/google&response_type=code&scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email';
-
-const kakao =
-  'https://kauth.kakao.com/oauth/authorize?client_id=cc1dfb0ff3ba6fc45a04a2771433fd75&redirect_uri=https://quiz-box.shop/oauth/kakao&response_type=code';
-
 export const Oauth = () => {
   return (
     <div className={cs.oauth}>
-      <a href={google} className={cx(cs.oauthBtn, cs.google)}>
+      <a href={String(process.env.REACT_APP_GOOGLE_LOGIN_REDIRECT_URL)} className={cx(cs.oauthBtn, cs.google)}>
         <GoogleLogin width='32px' height='32px' />
         Google
       </a>
-      <a href={kakao} className={cx(cs.oauthBtn, cs.kakao)}>
+      <a href={String(process.env.REACT_APP_KAKAO_LOGIN_REDIRECT_URL)} className={cx(cs.oauthBtn, cs.kakao)}>
         <KakaoLogin width='32px' height='32px' />
         Kakao
       </a>
