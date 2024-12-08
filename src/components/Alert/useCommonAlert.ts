43130a0a9d7bb282c8cfed5224c3146c453a79c9
solchan98 <dev.sol0427@gmail.com>
2024-12-08 18:53:47 +0900

    feat: alert timeout 설정 로직 수정 - 새로운 alert 등록될 때 마다 기존 setTimeout 제거 및 새로 등록하도록 로직 수정

--- src/components/Alert/useCommonAlert.ts
+++ src/components/Alert/useCommonAlert.ts
@@ -6,14 +6,17 @@ export interface IUseCommonAlertProps {
 }
 
 export const useCommonAlert = () => {
+  const [timerIdState, setTimerIdState] = useState<NodeJS.Timeout>();
   const [alertState, setAlertState] = useState<IUseCommonAlertProps>({ active: false, message: '' });
 
   const onAlert = (message: string) => {
+    clearTimeout(timerIdState);
     setAlertState((prev) => ({ ...prev, active: true, message }));
 
-    setTimeout(() => {
+    const timerId = setTimeout(() => {
       setAlertState((prev) => ({ ...prev, active: false, message: '' }));
     }, 3000);
+    setTimerIdState(timerId);
   };
 
   return { alertState, onAlert };
