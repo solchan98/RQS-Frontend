d2aaaa7a8158c91b354fe4b7bd7d1a547ac720ff
solchan98 <dev.sol0427@gmail.com>
2025-01-26 17:20:15 +0900

    chore: 우선순위 낮은 기능 주석 처리

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,12 +1,10 @@
-import { IoNotificationsOutline, IoSettingsOutline } from 'react-icons/io5';
-import { PiExam, PiPackage } from 'react-icons/pi';
+import { IoNotificationsOutline } from 'react-icons/io5';
 
 import React from 'react';
-import { AddedTopicsQuizContainer, HomeHeader, HomeHeaderIcons, HomeLayout, HomeLayoutContainer } from './index.styles';
+import { HomeHeader, HomeHeaderIcons, HomeLayout, HomeLayoutContainer } from './index.styles';
 import { NickName } from 'components/NickName/NickName';
 import { Icon } from 'components/Icon/Icon';
 import { OnGoingQuizGame } from './components/OnGoingQuizGame/OnGoingQuizGame';
-import { AddedTopicsQuiz } from './components/AddedTopicsQuiz/AddedTopicsQuiz';
 import { BlockCalender } from '../../components/BlockCalender/BlockCalender';
 import { Label } from '../../components/Label/Label';
 import { Button } from '@mui/material';
@@ -23,9 +21,9 @@ export const Home = () => {
           <Icon icon={IoNotificationsOutline} size={24} />
         </HomeHeaderIcons>
       </HomeHeader>
-      <AddedTopicsQuizContainer>
-        <AddedTopicsQuiz />
-      </AddedTopicsQuizContainer>
+      {/* <AddedTopicsQuizContainer> */}
+      {/*  <AddedTopicsQuiz /> */}
+      {/* </AddedTopicsQuizContainer> */}
       <div style={{ display: 'flex', gap: 12, justifyContent: 'space-between' }}>
         <HomeLayoutContainer style={{ width: '100%' }}>
           <Button type='button' onClick={() => navigate('/new-quizzes')}>
@@ -41,34 +39,34 @@ export const Home = () => {
       <HomeLayoutContainer>
         <OnGoingQuizGame />
       </HomeLayoutContainer>
-      <HomeLayoutContainer>
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Label title='11월 20일 수요일' />
-          <Icon icon={IoSettingsOutline} size={18} />
-        </div>
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <div style={{ display: 'flex' }}>
-              <Icon icon={PiPackage} size={24} />
-              <div style={{ display: 'flex', flexDirection: 'column' }}>
-                <span>8건</span>
-                <span>학습한지 7일이 지난 퀴즈팩</span>
-              </div>
-            </div>
-            <button type='button'>더보기</button>
-          </div>
-          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <div style={{ display: 'flex' }}>
-              <Icon icon={PiExam} size={24} />
-              <div style={{ display: 'flex', flexDirection: 'column' }}>
-                <span>12건</span>
-                <span>80점 이하 점수를 가진 퀴즈팩</span>
-              </div>
-            </div>
-            <button type='button'>더보기</button>
-          </div>
-        </div>
-      </HomeLayoutContainer>
+      {/* <HomeLayoutContainer> */}
+      {/*  <div style={{ display: 'flex', justifyContent: 'space-between' }}> */}
+      {/*    <Label title='11월 20일 수요일' /> */}
+      {/*    <Icon icon={IoSettingsOutline} size={18} /> */}
+      {/*  </div> */}
+      {/*  <div style={{ display: 'flex', flexDirection: 'column' }}> */}
+      {/*    <div style={{ display: 'flex', justifyContent: 'space-between' }}> */}
+      {/*      <div style={{ display: 'flex' }}> */}
+      {/*        <Icon icon={PiPackage} size={24} /> */}
+      {/*        <div style={{ display: 'flex', flexDirection: 'column' }}> */}
+      {/*          <span>8건</span> */}
+      {/*          <span>학습한지 7일이 지난 퀴즈팩</span> */}
+      {/*        </div> */}
+      {/*      </div> */}
+      {/*      <button type='button'>더보기</button> */}
+      {/*    </div> */}
+      {/*    <div style={{ display: 'flex', justifyContent: 'space-between' }}> */}
+      {/*      <div style={{ display: 'flex' }}> */}
+      {/*        <Icon icon={PiExam} size={24} /> */}
+      {/*        <div style={{ display: 'flex', flexDirection: 'column' }}> */}
+      {/*          <span>12건</span> */}
+      {/*          <span>80점 이하 점수를 가진 퀴즈팩</span> */}
+      {/*        </div> */}
+      {/*      </div> */}
+      {/*      <button type='button'>더보기</button> */}
+      {/*    </div> */}
+      {/*  </div> */}
+      {/* </HomeLayoutContainer> */}
       <HomeLayoutContainer>
         <Label title='My Study Timeline' size={14} />
         <BlockCalender />
